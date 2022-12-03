4b1a80f8ea858311db0ecf6a782d8c4214a0defa
Kamil Paszkowski <Kamil.Paszkowski@euvic.pl>
2022-12-03 12:08:09 +0100

    prepare exercise for http methods

---
+++ Euvic.WebAPI.HttpMethods/Controllers/Requests/UpdateTrainingRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Euvic.WebAPI.HttpMethods.Controllers.Requests
+{
+    public class UpdateTrainingRequest
+    {
+        public int StartDate { get; set; }
+        public int DurationInMinutes { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ Euvic.WebAPI.HttpMethods/Controllers/TrainingsController.cs
@@ -0,0 +1,32 @@
+﻿using Euvic.WebAPI.HttpMethods.Controllers.Requests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Euvic.WebAPI.HttpMethods.Controllers
+{
+    [Route("api/trainings")]
+    [ApiController]
+    public class TrainingsController : ControllerBase
+    {
+        // w każdym endpoincie zamień Route() na odpowiednia metodę http: HttpGet, HttpPost, HttpPut, HttpPatch, HttpDelete
+        [HttpGet("")]
+        public IActionResult GetTrainings() => Ok();
+
+        [Route("{id}/attendees")]
+        public IActionResult GetTrainingAttendees() => Ok();
+
+        [Route("")]
+        public IActionResult CreateTraining() => Ok();
+
+        [Route("{id}")]
+        public IActionResult DeleteTraining([FromRoute] int id) => Ok();
+
+        [Route("{id}/attendee")]
+        public IActionResult DeleteAttendee([FromRoute] int id) => Ok();
+
+        [Route("{id}")]
+        public IActionResult UpdateTraining([FromRoute] int id, [FromBody] UpdateTrainingRequest request) => Ok();
+
+        [Route("{id}/duration")]
+        public IActionResult ChangeTrainingDuration([FromRoute] int id, [FromBody] int durationInMinutes) => Ok();
+    }
+}
